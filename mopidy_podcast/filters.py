edd0455dffc5849d85af36eaf861bde5ca65c280
Thomas Kemmer <tkemmer@computer.org>
2014-02-05 07:26:40 +0100

    Refactor inexact match

--- mopidy_podcast/filters.py
+++ mopidy_podcast/filters.py
@@ -1,7 +1,20 @@
 from __future__ import unicode_literals
 
 
-def _match(q, value):
+# FIXME: split key: [values] into [(key, value), ...]
+# FIXME: value.lower() if not exact
+def _prepare_query(query, exact):
+    q = dict(query)
+    for (_, values) in q.iteritems():
+        if not values:
+            raise LookupError('Missing query')
+        for value in values:
+            if not value:
+                raise LookupError('Missing query')
+    return q
+
+
+def _match_value(q, value):
     return value and q.lower() in value.lower()
 
 
@@ -26,25 +39,25 @@ DEFAULT_FILTERS = dict.fromkeys(FILTER_FIELDS, lambda q, model: False)
 TRACK_FILTERS = [
     dict(
         DEFAULT_FILTERS,
-        uri=lambda q, track: _match(q, track.uri),
-        track_name=lambda q, track: _match(q, track.name),
-        album=lambda q, track: track.album and _match(q, track.album.name),
+        uri=lambda q, track: _match_value(q, track.uri),
+        track_name=lambda q, track: _match_value(q, track.name),
+        album=lambda q, track: track.album and _match_value(q, track.album.name),
         artist=lambda q, track: any(
-            _match(q, a.name) for a in track.artists
+            _match_value(q, a.name) for a in track.artists
         ),
         composer=lambda q, track: any(
-            _match(q, a.name) for a in track.composers
+            _match_value(q, a.name) for a in track.composers
         ),
         performer=lambda q, track: any(
-            _match(q, a.name) for a in track.performers
+            _match_value(q, a.name) for a in track.performers
         ),
         albumartist=lambda q, track: track.album and any(
-            _match(q, a.name) for a in track.album.artists
+            _match_value(q, a.name) for a in track.album.artists
         ),
         track_no=lambda q, track: int(q) == track.track_no,
-        genre=lambda q, track: _match(q, track.genre),
+        genre=lambda q, track: _match_value(q, track.genre),
         date=lambda q, track: track.date and track.date.startswith(q),
-        comment=lambda q, track: _match(q, track.comment)
+        comment=lambda q, track: _match_value(q, track.comment)
     ),
     dict(
         DEFAULT_FILTERS,
@@ -73,13 +86,13 @@ TRACK_FILTERS = [
 # TODO: any
 ALBUM_FILTERS = [
     dict(DEFAULT_FILTERS,
-         uri=lambda q, album: _match(q, album.uri),
-         album=lambda q, album: _match(q, album.name),
+         uri=lambda q, album: _match_value(q, album.uri),
+         album=lambda q, album: _match_value(q, album.name),
          artist=lambda q, album: any(
-             _match(q, a.name) for a in album.artists
+             _match_value(q, a.name) for a in album.artists
          ),
          albumartist=lambda q, album: any(
-             _match(q, a.name) for a in album.artists
+             _match_value(q, a.name) for a in album.artists
          ),
          date=lambda q, album: album.date and album.date.startswith(q)),
     dict(DEFAULT_FILTERS,
@@ -94,12 +107,12 @@ ALBUM_FILTERS = [
          date=lambda q, album: q == album.date)
 ]
 
-# FIXME: case insensitive 'in', strip q, any, albumartist
+# TODO: any
 ARTIST_FILTERS = [
     dict(
         DEFAULT_FILTERS,
-        uri=lambda q, artist: _match(q, artist.uri),
-        artist=lambda q, artist: _match(q, artist.name)
+        uri=lambda q, artist: _match_value(q, artist.uri),
+        artist=lambda q, artist: _match_value(q, artist.name)
     ),
     dict(
         DEFAULT_FILTERS,
@@ -116,15 +129,18 @@ def _get_filters(filters, query):
 
 
 def filter_tracks(tracks, query, exact=False):
+    query = _prepare_query(query, exact)
     filters = _get_filters(TRACK_FILTERS[exact], query)
     return filter(lambda t: all(f(t) for f in filters), tracks)
 
 
 def filter_albums(albums, query, exact=False):
+    query = _prepare_query(query, exact)
     filters = _get_filters(ALBUM_FILTERS[exact], query)
     return filter(lambda t: all(f(t) for f in filters), albums)
 
 
 def filter_artists(artists, query, exact=False):
+    query = _prepare_query(query, exact)
     filters = _get_filters(ARTIST_FILTERS[exact], query)
     return filter(lambda t: all(f(t) for f in filters), artists)
