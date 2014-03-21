cfc1405671ca45f29547eb339c1db8bdfb718a83
Thomas Kemmer <tkemmer@computer.org>
2014-03-21 06:23:46 +0100

    Update description

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     license='Apache License, Version 2.0',
     author='Thomas Kemmer',
     author_email='tkemmer@computer.org',
-    description='Mopidy extension for streaming podcasts',
+    description='Mopidy extension for searching and streaming podcasts',
     long_description=open('README.rst').read(),
     packages=find_packages(exclude=['tests', 'tests.*']),
     zip_safe=False,
